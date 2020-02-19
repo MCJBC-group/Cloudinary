07da7675564a0a5b80ce664960d93683dff2ac27
kristynf <kristynf@bu.edu>
2020-02-19 11:45:46 -0500

    initial commit

---
+++ src/main/java/com/kristyn/springboot503/ActorRepository.java
@@ -0,0 +1,6 @@
+package com.kristyn.springboot503;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long> {
+}
---
+++ src/test/java/com/kristyn/springboot503/Springboot503ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.springboot503;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot503ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
